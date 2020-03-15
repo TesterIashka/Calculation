cf0eeaf3c457e7c9bf8046f28ba93f5e002d2566
user <test.space.job@gmail.com>
2020-03-15 23:16:17 +0300

    change search min int+ and max int- value

--- src/main/java/SearchMinMaxValue.java
+++ src/main/java/SearchMinMaxValue.java
@@ -1,29 +1,49 @@
+import java.util.Arrays;
+
 public class SearchMinMaxValue {
     public static void main(String[] args) {
         int[] randomMassive = new int[20];
+        int maxNegatValue = -10;
+        int minPositValue = 10;
+        int minMax;
         int min = 0;
         int max = 0;
-        int minMax;
+        int mimin = 21;
+        int maxmax = 21;
+
         for (int i = 0; i < randomMassive.length; i++) {
             randomMassive[i] = (int) (Math.random()*(20+1)) - 10;
         }
+
         for (int i = 0; i < randomMassive.length; i++){
-            System.out.println(randomMassive[i]);
-            if (i < randomMassive.length-1){
-            if (randomMassive[min] > randomMassive[i+1])
-            {min = i+1;} else {
-                if (randomMassive[max] < randomMassive[i + 1]) {
-                    max = i + 1;
-                }
+            System.out.print(randomMassive[i] +" ");
+            if (randomMassive[i] < minPositValue && randomMassive[i] >= 0)
+            {
+                minPositValue = randomMassive[i];
+                mimin = i;
+            }
+            if (randomMassive[i] > maxNegatValue && randomMassive[i] < 0)
+            {
+                maxNegatValue = randomMassive[i];
+                maxmax = i;
+            }
+
+        }
+
+        try {
+            System.out.println("\nИзмененный массив после перестановки:");
+            System.out.println("Минимальное положительное число "+randomMassive[mimin] + "\n" +
+                    "Максимальное отрицательное число " + randomMassive[maxmax]);
+            minMax = randomMassive[mimin];
+            randomMassive[mimin] = randomMassive[maxmax];
+            randomMassive[maxmax] = minMax;
+            for (int value : randomMassive) {
+                System.out.print(value + " ");
             }
         }
-        }
-        System.out.println("--------------------------");
-        minMax = randomMassive[min];
-        randomMassive[min] = randomMassive[max];
-        randomMassive[max] = minMax;
-        for (int value : randomMassive) {
-            System.out.println(value);
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("В массиве не найдены либо положительные числа, либо отрицательные");
         }
 
     }
